82c1ec78c90b0dd037df563d388e31df0f24a624
Aimee Simmons <aimeesophiasimmons@gmail.com>
2021-07-25 13:10:39 +0100

    Added Customer API controller.

---
+++ OnionArchitecture/OnionArchitecture.UI/Controllers/CustomerController.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OnionArchitecture.Domain.Models;
+using OnionArchitecture.Services.CustomerService;
+
+namespace OnionArchitecture.UI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomerController : ControllerBase
+    {
+        private readonly ICustomerService _customerService;
+
+        public CustomerController(ICustomerService customerService)
+        {
+            _customerService = customerService;
+        }
+
+        [HttpGet(nameof(GetCustomer))]
+        public IActionResult GetCustomer(int id)
+        {
+            var result = _customerService.GetCustomer(id);
+
+            if (result is not null)
+            {
+                return Ok(result);
+            }
+
+            return BadRequest("No records found");
+        }
+
+        [HttpGet(nameof(GetAllCustomers))]
+        public IActionResult GetAllCustomers()
+        {
+            var result = _customerService.GetAllCustomers();
+
+            if (result is not null)
+            {
+                return Ok(result);
+            }
+
+            return BadRequest("No records found");
+        }
+
+        [HttpPost(nameof(InsertCustomer))]
+        public IActionResult InsertCustomer(Customer customer)
+        {
+            _customerService.InsertCustomer(customer);
+
+            return Ok("Customer inserted");
+        }
+
+        [HttpPut(nameof(UpdateCustomer))]
+        public IActionResult UpdateCustomer(Customer customer)
+        {
+            _customerService.UpdateCustomer(customer);
+
+            return Ok("Customer updated");
+        }
+
+        [HttpDelete(nameof(DeleteCustomer))]
+        public IActionResult DeleteCustomer(int id)
+        {
+            _customerService.DeleteCustomer(id);
+
+            return Ok("Customer deleted");
+        }
+    }
+}
